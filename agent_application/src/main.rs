fd9ddf7d090433817ee67d52dcacb1a7e491a766
nanderstabel <nander.stabel@impierce.com>
2023-11-28 12:47:39 +0100

    feat: prepare startup_events

--- agent_application/src/main.rs
+++ agent_application/src/main.rs
@@ -24,13 +24,16 @@ async fn main() {
 }
 
 async fn startup_events(state: ApplicationState<IssuanceData, IssuanceDataView>) {
-    let base_url: url::Url = "https://example.com/".parse().unwrap();
+    let base_url: url::Url = "http://0.0.0.0:3033/".parse().unwrap();
 
     match command_handler(
         "agg-id-F39A0C".to_string(),
         &state,
         IssuanceCommand::LoadCredentialFormatTemplate {
-            credential_format_template: serde_json::from_str(r#"{"foo":"bar"}"#).unwrap(),
+            credential_format_template: serde_json::from_str(include_str!(
+                "../../agent_issuance/res/credential_format_templates/openbadges_v3.json"
+            ))
+            .unwrap(),
         },
     )
     .await
@@ -45,7 +48,7 @@ async fn startup_events(state: ApplicationState<IssuanceData, IssuanceDataView>)
         IssuanceCommand::LoadAuthorizationServerMetadata {
             authorization_server_metadata: Box::new(AuthorizationServerMetadata {
                 issuer: base_url.clone(),
-                token_endpoint: Some(base_url.join("token").unwrap()),
+                token_endpoint: Some(base_url.join("v1/oauth/token").unwrap()),
                 ..Default::default()
             }),
         },
@@ -63,7 +66,7 @@ async fn startup_events(state: ApplicationState<IssuanceData, IssuanceDataView>)
             credential_issuer_metadata: CredentialIssuerMetadata {
                 credential_issuer: base_url.clone(),
                 authorization_server: None,
-                credential_endpoint: base_url.join("credential").unwrap(),
+                credential_endpoint: base_url.join("v1/openid4vci/credential").unwrap(),
                 deferred_credential_endpoint: None,
                 batch_credential_endpoint: None,
                 credentials_supported: vec![],
@@ -74,19 +77,6 @@ async fn startup_events(state: ApplicationState<IssuanceData, IssuanceDataView>)
     .await
     {
         Ok(_) => println!("Startup task completed: `LoadCredentialIssuerMetadata`"),
-        Err(err) => println!("Startup task failed: {:#?}", err),
-    };
-
-    match command_handler(
-        "agg-id-F39A0C".to_string(),
-        &state,
-        IssuanceCommand::CreateSubject {
-            pre_authorized_code: "adhjhdjajkdkhjhdj".to_string(),
-        },
-    )
-    .await
-    {
-        Ok(_) => println!("Startup task completed."),
         Err(err) => println!("Startup task failed: {:#?}", err),
     };
 
@@ -118,41 +108,7 @@ async fn startup_events(state: ApplicationState<IssuanceData, IssuanceDataView>)
     )
     .await
     {
-        Ok(_) => println!("Startup task completed."),
+        Ok(_) => println!("Startup task completed: `CreateCredentialsSupported`"),
         Err(err) => println!("Startup task failed: {:#?}", err),
     };
-
-    // match command_handler(
-    //     "agg-id-F39A0C".to_string(),
-    //     &state,
-    //     IssuanceCommand::CreateTokenResponse {
-    //         subject_id: uuid::Uuid::new_v4(),
-    //         token_request: TokenRequest::PreAuthorizedCode {
-    //             pre_authorized_code: "adhjhdjajkdkhjhdj".to_string(),
-    //             user_pin: None,
-    //         },
-    //     },
-    // )
-    // .await
-    // {
-    //     Ok(_) => println!("Startup task completed."),
-    //     Err(err) => println!("Startup task failed: {:#?}", err),
-    // };
-
-    // match command_handler(
-    //     "agg-id-F39A0C".to_string(),
-    //     &state,
-    //     IssuanceCommand::CreateTokenResponse {
-    //         subject_id: uuid::Uuid::new_v4(),
-    //         token_request: TokenRequest::PreAuthorizedCode {
-    //             pre_authorized_code: "adhjhdjajkdkhjhdj".to_string(),
-    //             user_pin: None,
-    //         },
-    //     },
-    // )
-    // .await
-    // {
-    //     Ok(_) => println!("Startup task completed."),
-    //     Err(err) => println!("Startup task failed: {:#?}", err),
-    // };
 }
