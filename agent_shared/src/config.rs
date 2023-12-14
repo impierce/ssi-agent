24a78c5c8de7479eed6fbdde1c51613b8d547a91
nanderstabel <nander.stabel@impierce.com>
2023-12-14 14:08:39 +0100

    feat: add `test` feature to `agent_shared` to allow for testing across agents

--- agent_shared/src/config.rs
+++ agent_shared/src/config.rs
@@ -2,17 +2,31 @@ use tracing::info;
 
 /// Read environment variables
 pub fn config(package_name: &str) -> config::Config {
-    // Load global .env file
-    dotenvy::dotenv().ok();
+    let config = if cfg!(feature = "test") {
+        test_config()
+    } else {
+        dotenvy::dotenv().ok();
 
-    // Build configuration
-    let config = config::Config::builder()
-        .add_source(config::Environment::with_prefix(package_name))
-        .add_source(config::Environment::with_prefix("AGENT_CONFIG"))
-        .build()
-        .unwrap();
+        config::Config::builder()
+            .add_source(config::Environment::with_prefix(package_name))
+            .add_source(config::Environment::with_prefix("AGENT_CONFIG"))
+            .build()
+            .unwrap()
+    };
 
     info!("{:?}", config);
 
     config
+}
+
+/// Read environment variables for tests that can be used across packages
+#[cfg(feature = "test")]
+fn test_config() -> config::Config {
+    dotenvy::from_filename("agent_shared/tests/.env.test").ok();
+
+    config::Config::builder()
+        .add_source(config::Environment::with_prefix("TEST"))
+        .add_source(config::Environment::with_prefix("AGENT_CONFIG"))
+        .build()
+        .unwrap()
 }
--- agent_shared/tests/config.rs
+++ agent_shared/tests/config.rs
@@ -1,9 +1,8 @@
 use agent_shared::config;
 
+#[cfg(feature = "test")]
 #[test]
 fn test_config() {
-    dotenvy::from_filename("tests/.env.test").ok();
-
     assert_eq!(config!("variable").unwrap(), "env_value");
     assert_eq!(config!("global_variable").unwrap(), "global_env_value");
     // Reading from an environment variable that belongs to another package should fail.
