a0e581756e4ed44f9491af11377e38197dcdf6f8
nanderstabel <nander.stabel@impierce.com>
2023-12-14 14:09:28 +0100

    test: use the `agent_shared` `test` feature

--- agent_api_rest/src/credential_issuer/well_known/openid_credential_issuer.rs
+++ agent_api_rest/src/credential_issuer/well_known/openid_credential_issuer.rs
@@ -35,12 +35,23 @@ mod tests {
         startup_commands::{create_credentials_supported, load_credential_issuer_metadata},
         state::{initialize, CQRS},
     };
+    use agent_shared::config;
     use agent_store::in_memory;
     use axum::{
         body::Body,
         http::{self, Request},
     };
-    use serde_json::{json, Value};
+    use oid4vci::{
+        credential_format_profiles::{
+            w3c_verifiable_credentials::jwt_vc_json::{CredentialDefinition, JwtVcJson},
+            CredentialFormats, Parameters,
+        },
+        credential_issuer::{
+            credential_issuer_metadata::CredentialIssuerMetadata, credentials_supported::CredentialsSupportedObject,
+        },
+        ProofType,
+    };
+    use serde_json::json;
     use tower::ServiceExt;
 
     #[tokio::test]
@@ -73,31 +84,41 @@ mod tests {
         assert_eq!(response.status(), StatusCode::OK);
 
         let body = hyper::body::to_bytes(response.into_body()).await.unwrap();
-        let body: Value = serde_json::from_slice(&body).unwrap();
+        let cfredential_issuer_metadata: CredentialIssuerMetadata = serde_json::from_slice(&body).unwrap();
         assert_eq!(
-            body,
-            json!({
-                "credential_issuer": "https://example.com/",
-                "credential_endpoint": "https://example.com/openid4vci/credential",
-                "credentials_supported": [{
-                    "format": "jwt_vc_json",
-                    "cryptographic_binding_methods_supported": [
-                        "did:key"
-                    ],
-                    "cryptographic_suites_supported": [
-                        "EdDSA"
-                    ],
-                    "credential_definition":{
-                        "type": [
-                            "VerifiableCredential",
-                            "OpenBadgeCredential"
-                        ]
-                    },
-                    "proof_types_supported": [
-                        "jwt"
-                    ]
-                }]
-            })
+            cfredential_issuer_metadata,
+            CredentialIssuerMetadata {
+                credential_issuer: BASE_URL.clone(),
+                authorization_server: None,
+                credential_endpoint: BASE_URL.join("openid4vci/credential").unwrap(),
+                batch_credential_endpoint: None,
+                deferred_credential_endpoint: None,
+                credentials_supported: vec![CredentialsSupportedObject {
+                    id: None,
+                    credential_format: CredentialFormats::JwtVcJson(Parameters {
+                        format: JwtVcJson,
+                        parameters: (
+                            CredentialDefinition {
+                                type_: vec!["VerifiableCredential".to_string(), "OpenBadgeCredential".to_string()],
+                                credential_subject: None,
+                            },
+                            None,
+                        )
+                            .into(),
+                    }),
+                    scope: None,
+                    cryptographic_binding_methods_supported: Some(vec!["did:key".to_string()]),
+                    cryptographic_suites_supported: Some(vec!["EdDSA".to_string()]),
+                    proof_types_supported: Some(vec![ProofType::Jwt]),
+                    display: Some(vec![json!({
+                       "name": config!("credential_name").unwrap(),
+                       "logo": {
+                            "url": config!("credential_logo_url").unwrap()
+                       }
+                    })]),
+                }],
+                display: None,
+            }
         );
     }
 }
