ea22279e8175b2d6c811489228265199a6770ee3
joris <joris.willems@impierce.com>
2024-02-08 14:04:18 +0100

    updating portal-ui

--- portal-ui/src/lib/editor.ts
+++ portal-ui/src/lib/editor.ts
@@ -1,17 +1,8 @@
-export async function fetchOAuthServer() {
+export async function fetchOAuthServer(): Promise<Response> {
     const result = await fetch("http://192.168.2.12:3033/.well-known/oauth-authorization-server");
-    
+
     console.log("oauth-authorization-server", result);
 
-    createJsonEditor(result);
-
+    return result;
 }
 
-export function createJsonEditor(data: any) {
-    const body = new HTMLDivElement();
-    body.classList.add("json-editor");
-
-    for (const [key, val] of Object.entries(data)) {
-        console.log(key, val);
-    }
-}
--- portal-ui/src/routes/+page.svelte
+++ portal-ui/src/routes/+page.svelte
@@ -1,5 +1,20 @@
 <script lang="ts">
-    import {fetchOAuthServer} from "$lib/editor";
+    import { onMount } from "svelte";
+
+    onMount(async() => {
+        const result = await fetch("http://192.168.2.12:3033/.well-known/oauth-authorization-server");
+
+        console.log("oauth-authorization-server", result);
+
+        const body = document.createElement("div");
+
+        body.classList.add("json-editor");
+
+        for (const [key, val] of Object.entries(result)) {
+            console.log(key, val);
+        }
+    })
+
 </script>
 
 <h1>Portal Unicore</h1>
