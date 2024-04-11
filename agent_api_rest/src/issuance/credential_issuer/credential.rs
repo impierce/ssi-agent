fdbf1d4ef2eafd667f9e65aad9cc30cbaecad61e
Nander Stabel <nander.stabel@impierce.com>
2024-04-11 14:32:19 +0200

    test: add Content-Type assertions to `agent_api_rest` tests

--- agent_api_rest/src/issuance/credential_issuer/credential.rs
+++ agent_api_rest/src/issuance/credential_issuer/credential.rs
@@ -152,6 +152,7 @@ mod tests {
             .unwrap();
 
         assert_eq!(response.status(), StatusCode::OK);
+        assert_eq!(response.headers().get("Content-Type").unwrap(), "application/json");
 
         let body = axum::body::to_bytes(response.into_body(), usize::MAX).await.unwrap();
         let body: Value = serde_json::from_slice(&body).unwrap();
--- agent_api_rest/src/issuance/credential_issuer/token.rs
+++ agent_api_rest/src/issuance/credential_issuer/token.rs
@@ -91,6 +91,7 @@ pub mod tests {
             .unwrap();
 
         assert_eq!(response.status(), StatusCode::OK);
+        assert_eq!(response.headers().get("Content-Type").unwrap(), "application/json");
 
         let body = axum::body::to_bytes(response.into_body(), usize::MAX).await.unwrap();
         let token_response: TokenResponse = serde_json::from_slice(&body).unwrap();
