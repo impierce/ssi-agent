aa636e0f3ac182416458cd6604f15e3948f96a18
Daniel Mader <daniel.mader@impierce.com>
2025-03-11 00:25:54 +0100

    ci: add `APP_VERSION`, `GIT_COMMIT_HASH` to Docker build

--- agent_application/src/metadata/version.rs
+++ agent_application/src/metadata/version.rs
@@ -1,17 +1,19 @@
 use axum::Json;
 use serde::Serialize;
+use serde_with::skip_serializing_none;
 
+#[skip_serializing_none]
 #[derive(Serialize)]
 pub struct Version {
-    version: String,
-    git_commit_hash: String,
+    version: Option<String>,
+    git_commit_hash: Option<String>,
 }
 
 /// Returns the `version` and the `git_commit_hash` of the application.
 pub async fn version() -> Json<Version> {
     let version = Version {
-        version: std::env::var("UNICORE__APP_VERSION").unwrap_or_else(|_| "unknown".to_string()),
-        git_commit_hash: std::env::var("UNICORE__APP_GIT_COMMIT_HASH").unwrap_or_else(|_| "unknown".to_string()),
+        version: std::env::var("APP_VERSION").ok(),
+        git_commit_hash: std::env::var("GIT_COMMIT_HASH").ok(),
     };
     Json(version)
 }
