b61c5fccdb4578f251d0796f272d95d92b6b2d6d
Daniel Mader <daniel.mader@impierce.com>
2025-03-10 23:04:01 +0100

    feat: add endpoints to Postman, update docstring

--- agent_application/src/metadata/info.rs
+++ agent_application/src/metadata/info.rs
@@ -7,13 +7,13 @@ use super::MetadataState;
 #[derive(Serialize)]
 pub struct Info {
     version: String,
-    git_commit: String,
+    git_commit_hash: String,
     release_channel: String, // tbd: stable/latest (main), next, beta, canary (alpha)
     docker_build_timestamp: String,
     uptime: String,
 }
 
-/// A simple liveness probe following application monitoring conventions.
+/// Returns the `version`, application uptime and a few more metadata values.
 pub async fn info(State(state): State<MetadataState>) -> Json<Info> {
     let time_delta = TimeDelta::seconds(state.startup_instant.elapsed().as_secs() as i64);
     let uptime_human_readable = format!(
@@ -25,7 +25,7 @@ pub async fn info(State(state): State<MetadataState>) -> Json<Info> {
     );
     let info = Info {
         version: std::env::var("UNICORE__APP_VERSION").unwrap_or_else(|_| "unknown".to_string()),
-        git_commit: std::env::var("UNICORE__APP_GIT_COMMIT").unwrap_or_else(|_| "unknown".to_string()),
+        git_commit_hash: std::env::var("UNICORE__APP_GIT_COMMIT_HASH").unwrap_or_else(|_| "unknown".to_string()),
         release_channel: std::env::var("UNICORE__APP_RELEASE_CHANNEL").unwrap_or_else(|_| "unknown".to_string()),
         docker_build_timestamp: std::env::var("UNICORE__APP_DOCKER_BUILD_DATE")
             .unwrap_or_else(|_| "unknown".to_string()),
--- agent_application/src/metadata/version.rs
+++ agent_application/src/metadata/version.rs
@@ -4,19 +4,14 @@ use serde::Serialize;
 #[derive(Serialize)]
 pub struct Version {
     version: String,
-    git_commit: String,
-    // release_channel: String,
-    // docker_build_timestamp: String,
+    git_commit_hash: String,
 }
 
-/// A simple liveness probe following application monitoring conventions.
+/// Returns the `version` and the `git_commit_hash` of the application.
 pub async fn version() -> Json<Version> {
     let version = Version {
         version: std::env::var("UNICORE__APP_VERSION").unwrap_or_else(|_| "unknown".to_string()),
-        git_commit: std::env::var("UNICORE__APP_GIT_COMMIT").unwrap_or_else(|_| "unknown".to_string()),
-        // release_channel: std::env::var("UNICORE__APP_RELEASE_CHANNEL").unwrap_or_else(|_| "unknown".to_string()),
-        // docker_build_timestamp: std::env::var("UNICORE__APP_DOCKER_BUILD_DATE")
-        //     .unwrap_or_else(|_| "unknown".to_string()),
+        git_commit_hash: std::env::var("UNICORE__APP_GIT_COMMIT_HASH").unwrap_or_else(|_| "unknown".to_string()),
     };
     Json(version)
 }
