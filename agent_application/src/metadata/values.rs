f9b163b0cb5ff43f648eed92e3e39cb03189b529
Daniel Mader <daniel.mader@impierce.com>
2025-03-11 01:51:58 +0100

    chore: check in generated file

---
+++ agent_application/src/metadata/values.rs
@@ -0,0 +1,4 @@
+// THIS FILE IS OVERWRITTEN BY A BUILD SCRIPT
+pub const APP_VERSION: Option<&str> = None;
+pub const GIT_COMMIT_HASH: Option<&str> = None;
+pub const DOCKER_BUILD_TIMESTAMP: Option<&str> = None;
