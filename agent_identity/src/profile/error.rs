5a7b04bacecdd9506eb26f912a1bfc0a339d435c
Nander Stabel <nander.stabel@impierce.com>
2025-07-29 13:28:27 +0200

    fix: update error message

--- agent_identity/src/profile/error.rs
+++ agent_identity/src/profile/error.rs
@@ -2,6 +2,6 @@ use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum ProfileError {
-    #[error("The resource cannot be modified at runtime because it was provisioned by a static configuration file.")]
+    #[error("The resource cannot be modified at runtime because it was provisioned")]
     ConfigurationConflict,
 }
