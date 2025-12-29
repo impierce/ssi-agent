aab2264b1268447a9608cdff7f55c476038efa31
Nander Stabel <nander.stabel@impierce.com>
2025-12-29 17:33:32 +0100

    fix: update signing algorithm for ES256 and enhance ManagedKey event handling

--- agent_identity/src/managed_key/views/mod.rs
+++ agent_identity/src/managed_key/views/mod.rs
@@ -8,8 +8,32 @@ impl View<ManagedKey> for ManagedKey {
     fn update(&mut self, event: &EventEnvelope<ManagedKey>) {
         use crate::managed_key::event::ManagedKeyEvent::*;
 
-        // match &event.payload {
-        //     _ => todo!(),
-        // }
+        match &event.payload {
+            KeyGenerated {
+                managed_key_id,
+                key_id,
+                alias,
+                signing_algorithm,
+            } => {
+                self.managed_key_id.clone_from(managed_key_id);
+                self.key_id.clone_from(key_id);
+                self.alias.clone_from(alias);
+                self.signing_algorithm.replace(signing_algorithm.clone());
+            }
+            KeyRemoved { managed_key_id: _ } => {
+                // Do not reset the entire state so that the removal can be undone if needed?
+                // *self = Self::default();
+                self.is_removed = true;
+            }
+            KeyAliasUpdated {
+                managed_key_id: _,
+                new_alias,
+            } => {
+                self.alias.clone_from(new_alias);
+            }
+            SigningKeySet { managed_key_id: _ } => {
+                self.is_signing_key = true;
+            }
+        }
     }
 }
