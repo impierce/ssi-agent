92710ecc1fdc5ef6c38d5d77969dcbd733c14279
Daniel Mader <daniel.mader@impierce.com>
2026-02-19 11:26:56 +0100

    refactor: provide explicit name with `as =` config option

--- agent_api_http/src/v0/templates/mod.rs
+++ agent_api_http/src/v0/templates/mod.rs
@@ -21,7 +21,7 @@ pub mod openapi;
 /// Data transfer object for Templates.
 #[derive(Serialize, Deserialize, utoipa::ToSchema)]
 #[serde(rename_all = "camelCase")]
-#[schema(title = "Template")]
+#[schema(as = Template)]
 pub struct TemplateDto {
     #[serde(rename = "id")]
     pub template_id: String,
