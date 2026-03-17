c8c14bf9a52aee078c383615cc3d380967bd8836
Nander Stabel <nander.stabel@impierce.com>
2026-03-17 02:47:07 +0100

    feat: initialize shared kernel

---
+++ shared-kernel/src/lib.rs
@@ -0,0 +1,16 @@
+// pub mod application_service;
+// pub mod command_handler;
+// pub mod custom_queries;
+// pub mod service_registry;
+// pub mod view_repository;
+
+// #[cfg(feature = "test-utils")]
+// pub mod test_utils;
+
+// Re-export commonly used items for convenience.
+pub use async_trait::async_trait;
+pub use chrono;
+pub use convert_case;
+pub use cqrs_es;
+pub use strum;
+pub use uuid::Uuid;
