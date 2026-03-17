953d366c486ae478394102894da6dc3ac78b79eb
Daniel Mader <daniel.mader@impierce.com>
2026-03-17 20:47:09 +0100

    style: clippy issue

--- agent_api_http/src/v0/identity/connections/mod.rs
+++ agent_api_http/src/v0/identity/connections/mod.rs
@@ -111,9 +111,7 @@ pub(crate) async fn get_connections(
                     display
                         .as_ref()
                         .map_or(true, |display| connection.display.as_ref() == Some(display))
-                        && url
-                            .as_ref()
-                            .map_or(true, |url| connection.url.to_string() == url.to_string())
+                        && url.as_ref().map_or(true, |url| connection.url == url)
                         && did.as_ref().map_or(true, |did| connection.dids.contains(did))
                 })
                 .collect();
