d1f09b36a3b8ef6868179cc1abf13eb69cff8592
Nander Stabel <nander.stabel@impierce.com>
2026-03-17 02:58:33 +0100

    feat:  add shared-kernel module

--- shared-kernel/src/lib.rs
+++ shared-kernel/src/lib.rs
@@ -1,12 +1,3 @@
-// pub mod application_service;
-// pub mod command_handler;
-// pub mod custom_queries;
-// pub mod service_registry;
-// pub mod view_repository;
-
-// #[cfg(feature = "test-utils")]
-// pub mod test_utils;
-
 // Re-export commonly used items for convenience.
 pub use async_trait::async_trait;
 pub use chrono;
