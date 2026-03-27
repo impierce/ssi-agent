a537171bf72b504d91c04118adab4a39f0fd2387
Nander Stabel <nander.stabel@impierce.com>
2026-03-27 16:21:39 +0100

    feat: add `description` field to Profile (#313)

--- agent_shared/src/config/mod.rs
+++ agent_shared/src/config/mod.rs
@@ -281,6 +281,7 @@ pub struct ApplicationConfiguration {
             Display {
                 name: "UniCore".to_string(),
                 locale: Some("en".to_string()),
+                description: None,
                 logo: Some(Logo {
                     uri: Some(Url::parse("https://www.impierce.com/external/impierce-icon.png").unwrap()),
                     alt_text: Some("Impierce Icon".to_string()),
@@ -556,6 +557,7 @@ pub struct Logo {
 #[derive(Serialize, Deserialize, Debug, Clone, Default)]
 pub struct Display {
     pub name: String,
+    pub description: Option<String>,
     pub locale: Option<String>,
     pub logo: Option<Logo>,
     pub country: Option<String>,
@@ -674,6 +676,7 @@ pub enum DocumentEvent {
 pub enum ProfileEvent {
     ProfileCreated,
     DisplayNameUpdated,
+    DescriptionUpdated,
     LogoUpdated,
     CountryUpdated,
     SourceUpdated,
--- agent_verification/src/authorization_request/aggregate.rs
+++ agent_verification/src/authorization_request/aggregate.rs
@@ -74,6 +74,7 @@ impl Aggregate for AuthorizationRequest {
                     if let ClientMetadataResource::ClientMetadata {
                         ref mut client_name,
                         ref mut logo_uri,
+                        ref mut other,
                         ..
                     } = oid4vp_client_metadata
                     {
@@ -84,6 +85,10 @@ impl Aggregate for AuthorizationRequest {
                             .display
                             .first()
                             .and_then(|d| d.logo.as_ref().and_then(|l| l.uri.clone()));
+
+                        if let Some(description) = config().display.first().and_then(|d| d.description.clone()) {
+                            other.insert("description".to_string(), description.into());
+                        }
                     }
 
                     client_id = format!("decentralized_identifier:{}", verifier_did);
@@ -95,7 +100,7 @@ impl Aggregate for AuthorizationRequest {
                             .scope(Scope::openid())
                             .response_uri(redirect_uri)
                             .response_mode("direct_post".to_string())
-                            .client_metadata(services.oid4vp_client_metadata.clone())
+                            .client_metadata(oid4vp_client_metadata)
                             .state(state)
                             .nonce(nonce)
                             .build()
@@ -106,6 +111,7 @@ impl Aggregate for AuthorizationRequest {
                     if let ClientMetadataResource::ClientMetadata {
                         ref mut client_name,
                         ref mut logo_uri,
+                        ref mut other,
                         ..
                     } = siopv2_client_metadata
                     {
@@ -116,6 +122,10 @@ impl Aggregate for AuthorizationRequest {
                             .display
                             .first()
                             .and_then(|d| d.logo.as_ref().and_then(|l| l.uri.clone()));
+
+                        if let Some(description) = config().display.first().and_then(|d| d.description.clone()) {
+                            other.insert("description".to_string(), description.into());
+                        }
                     }
 
                     client_id = verifier_did;
