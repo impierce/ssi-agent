e0e8fad3841e11d58c1feb00ee840c1e57e00adc
Nander Stabel <nander.stabel@impierce.com>
2026-06-10 18:26:20 +0200

    fix(api-http): update request_actor to handle nested Option for Actor

--- agent_api_http/src/handlers.rs
+++ agent_api_http/src/handlers.rs
@@ -4,14 +4,16 @@ use cqrs_es::{persist::ViewRepository, Aggregate, View};
 use shared_kernel::authorization::{Actor, AllowAllAuthorizationChecker, AuthorizationChecker};
 use std::sync::Arc;
 
-pub fn request_actor(actor: &Option<Extension<Option<Actor>>>) -> Option<Actor> {
-    actor.as_ref().and_then(|Extension(actor)| actor.clone())
+// Outer Option indicates whether the Extension is present (authorization enabled).
+// Inner Option indicates whether an Actor is present.
+pub fn request_actor(actor: &Option<Extension<Option<Actor>>>) -> Option<Option<Actor>> {
+    actor.as_ref().map(|Extension(actor)| actor.clone())
 }
 
 /// Wrapping the `command_handler` function from the `agent_shared` crate to handle errors.
 pub async fn command_handler<A>(
     authorization_checker: Arc<dyn AuthorizationChecker>,
-    actor: Option<Actor>,
+    actor: Option<Option<Actor>>,
     aggregate_id: &str,
     state: &agent_shared::application_state::CommandHandler<A>,
     command: <A as cqrs_es::Aggregate>::Command,
@@ -20,9 +22,19 @@ where
     A: Aggregate,
     <A as Aggregate>::Command: Send + Sync + std::fmt::Debug,
 {
-    if actor.is_none() {
-        return Err(ErrorWrapper::Unauthorized);
-    }
+    // if actor.is_none() {
+    //     return Err(ErrorWrapper::Unauthorized);
+    // }
+
+    let actor = if let Some(actor) = actor {
+        if actor.is_none() {
+            return Err(ErrorWrapper::Unauthorized);
+        }
+
+        actor
+    } else {
+        None
+    };
 
     agent_shared::handlers::command_handler(authorization_checker, actor, aggregate_id, state, command)
         .await
@@ -67,7 +79,7 @@ where
 // Wrapping the `query_handler` function from the `agent_shared` crate to handle errors.
 pub async fn query_handler<A, V>(
     authorization_checker: Arc<dyn AuthorizationChecker>,
-    actor: Option<Actor>,
+    actor: Option<Option<Actor>>,
     view_id: &str,
     state: &Arc<dyn ViewRepository<V, A>>,
 ) -> Result<Option<V>, ErrorWrapper<A::Error>>
@@ -75,9 +87,19 @@ where
     A: Aggregate,
     V: View<A>,
 {
-    if actor.is_none() {
-        return Err(ErrorWrapper::Unauthorized);
-    }
+    // if actor.is_none() {
+    //     return Err(ErrorWrapper::Unauthorized);
+    // }
+
+    let actor = if let Some(actor) = actor {
+        if actor.is_none() {
+            return Err(ErrorWrapper::Unauthorized);
+        }
+
+        actor
+    } else {
+        None
+    };
 
     agent_shared::handlers::query_handler(authorization_checker, actor, view_id, state)
         .await
