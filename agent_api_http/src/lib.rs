a0832cd8be6a36cb75a9bb2b27f0018a4b494940
Daniel Mader <daniel.mader@impierce.com>
2026-06-23 16:53:38 +0200

    fix: enable `issuance` router

--- agent_api_http/src/lib.rs
+++ agent_api_http/src/lib.rs
@@ -64,11 +64,12 @@ pub fn app(
                 .map(v0::authorization::router)
                 .unwrap_or_default(),
         )
-        // .merge(
-        //     issuance_state
-        //         .map(|issuance_state| v0::issuance::router((issuance_state, library_state.clone())))
-        //         .unwrap_or_default(),
-        // )
+        .merge(
+            issuance_state
+                .zip(library_state.clone())
+                .map(v0::issuance::router)
+                .unwrap_or_default(),
+        )
         .merge(holder_state.map(v0::holder::router).unwrap_or_default())
         .merge(verification_state.map(v0::verification::router).unwrap_or_default())
         .merge(public::router())
