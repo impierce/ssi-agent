aafcf2e7aa1985b42954561dddbedcb062a45f35
Daniel Mader <daniel.mader@impierce.com>
2026-07-10 09:34:30 +0200

    docs: add OpenAPI definition for credential presentations (#372)

--- agent_api_http/src/v0/holder/openapi.rs
+++ agent_api_http/src/v0/holder/openapi.rs
@@ -1,10 +1,23 @@
 use crate::v0::holder::holder::credentials::{__path_credential, __path_credentials};
 use crate::v0::holder::holder::offers::{__path_offer, __path_offers, accept::__path_accept, reject::__path_reject};
+use crate::v0::holder::holder::presentations::{
+    __path_get_presentations, __path_post_presentations, __path_presentation,
+};
 use utoipa::OpenApi;
 
 #[derive(OpenApi)]
 #[openapi(
-    paths(credential, credentials, offer, offers, accept, reject),
+    paths(
+        credential,
+        credentials,
+        offer,
+        offers,
+        accept,
+        reject,
+        get_presentations,
+        post_presentations,
+        presentation
+    ),
     tags(
         (name = "Identity", description = "Manage all aspects of your organisational identity."),
         (name = "Holder", description = "Manage the credentials your organisation holds itself."),
